cecb47719dace94905e24ea967cef038e56d1d4f
Timber <contactmindsek@gmail.com>
2025-02-26 14:24:27 +0100

    refactor: edit main, app module and eslint, add config package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
+  if (!process.env.JWT_SECRET) {
+    logger.error('JWT_SECRET environment variable is not defined!');
+    logger.warn('Using fallback secret - NOT SECURE FOR PRODUCTION');
+  } else {
+    logger.log('JWT_SECRET environment variable is defined!');
+  }
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('PriceBuddy API')
+    .setDescription('API pour PriceBuddy')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001, '0.0.0.0');
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
